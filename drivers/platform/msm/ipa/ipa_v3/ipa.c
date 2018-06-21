899682c13f8e0f543de5ef1ae4a0e401e51613c6
Demon Singur <demonsingur@gmail.com>
2018-06-21 06:44:04 +0000

    ipa_v3: block ipa_ws wakelocks
    Change-Id: I984776eb1ef9b8629be0bde7b89f0f3a134d1c53
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/platform/msm/ipa/ipa_v3/ipa.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa.c
@@ -4181,6 +4181,7 @@ void ipa3_dec_client_disable_clks_no_block(
  */
 void ipa3_inc_acquire_wakelock(void)
 {
+#if 0
 	unsigned long flags;
 
 	spin_lock_irqsave(&ipa3_ctx->wakelock_ref_cnt.spinlock, flags);
@@ -4188,6 +4189,7 @@ void ipa3_inc_acquire_wakelock(void)
 	IPADBG_LOW("active wakelock ref cnt = %d\n",
 		ipa3_ctx->wakelock_ref_cnt.cnt);
 	spin_unlock_irqrestore(&ipa3_ctx->wakelock_ref_cnt.spinlock, flags);
+#endif
 }
 
 /**
@@ -4200,6 +4202,7 @@ void ipa3_inc_acquire_wakelock(void)
  */
 void ipa3_dec_release_wakelock(void)
 {
+#if 0
 	unsigned long flags;
 
 	spin_lock_irqsave(&ipa3_ctx->wakelock_ref_cnt.spinlock, flags);
@@ -4207,6 +4210,7 @@ void ipa3_dec_release_wakelock(void)
 	IPADBG_LOW("active wakelock ref cnt = %d\n",
 		ipa3_ctx->wakelock_ref_cnt.cnt);
 	spin_unlock_irqrestore(&ipa3_ctx->wakelock_ref_cnt.spinlock, flags);
+#endif
 }
 
 int ipa3_set_clock_plan_from_pm(int idx)
@@ -5707,9 +5711,11 @@ static int ipa3_pre_init(const struct ipa3_plat_drv_res *resource_p,
 		goto fail_device_create;
 	}
 
+#if 0
 	/* Create a wakeup source. */
 	wakeup_source_init(&ipa3_ctx->w_lock, "IPA_WS");
 	spin_lock_init(&ipa3_ctx->wakelock_ref_cnt.spinlock);
+#endif
 
 	/* Initialize Power Management framework */
 	if (ipa3_ctx->use_ipa_pm) {
