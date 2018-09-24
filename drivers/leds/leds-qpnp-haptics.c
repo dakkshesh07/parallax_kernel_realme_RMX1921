4f34e0c8e569cf84a172c25f04cc39c4975e6814
flar2 <asegaert@gmail.com>
2018-09-23 20:47:30 -0400

    leds-qpnp-haptics: fix merge error
    Signed-off-by: khusika <khusikadhamar@gmail.com>
    Signed-off-by: ktommy91 <kemenesi.tamas@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/leds/leds-qpnp-haptics.c
+++ drivers/leds/leds-qpnp-haptics.c
@@ -2724,6 +2724,8 @@ static int qpnp_haptics_probe(struct platform_device *pdev)
 		}
 	}
 
+	gchip = chip;
+
 	return 0;
 
 sysfs_fail:
@@ -2755,8 +2757,6 @@ static int qpnp_haptics_remove(struct platform_device *pdev)
 	if (chip->pwm_data.pwm_dev)
 		pwm_put(chip->pwm_data.pwm_dev);
 	dev_set_drvdata(&pdev->dev, NULL);
-
-	gchip = chip;
 
 	return 0;
 }
