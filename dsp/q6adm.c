a197c5983aa38a9bf21fd38dec24d7f155474b53
Dieter Luecking <dieterl@codeaurora.org>
2018-11-14 15:11:47 +0100

    dsp: fix configuration of EC reference
    The check open_v8.endpoint_id_2 != 0xFFFF can only
    be true if changed after initializing open_v8.endpoint_id_2
    to 0xFFFF. If not, EC reference will never be configured
    correctly. Fixing code to account for that.
    
    Change-Id: I8d2aba19ae1f60ace118c828a98d5fecb149be18
    Signed-off-by: Dieter Luecking <dieterl@codeaurora.org>
    Signed-off-by: Avinash Chandra <avicha@codeaurora.org>

--- dsp/q6adm.c
+++ dsp/q6adm.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2020, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -3135,6 +3135,10 @@ int adm_open(int port_id, int path, int rate, int channel_mode, int topology,
 			open_v8.endpoint_id_2 = 0xFFFF;
 			open_v8.endpoint_id_3 = 0xFFFF;
 
+			if (this_adm.ec_ref_rx && (path != ADM_PATH_PLAYBACK)) {
+				open_v8.endpoint_id_2 = this_adm.ec_ref_rx;
+				this_adm.ec_ref_rx = -1;
+			}
 
 			open_v8.topology_id = topology;
 			open_v8.reserved = 0;
@@ -3165,11 +3169,8 @@ int adm_open(int port_id, int path, int rate, int channel_mode, int topology,
 			if ((this_adm.num_ec_ref_rx_chans != 0)
 				&& (path != ADM_PATH_PLAYBACK)
 				&& (open_v8.endpoint_id_2 != 0xFFFF)) {
-				open_v8.endpoint_id_2 = this_adm.ec_ref_rx;
-				this_adm.ec_ref_rx = -1;
 				ep2_payload.dev_num_channel =
 					this_adm.num_ec_ref_rx_chans;
-				this_adm.num_ec_ref_rx_chans = 0;
 
 				if (this_adm.ec_ref_rx_bit_width != 0) {
 					ep2_payload.bit_width =
@@ -3215,6 +3216,7 @@ int adm_open(int port_id, int path, int rate, int channel_mode, int topology,
 			if ((this_adm.num_ec_ref_rx_chans != 0)
 				&& (path != ADM_PATH_PLAYBACK)
 				&& (open_v8.endpoint_id_2 != 0xFFFF)) {
+				this_adm.num_ec_ref_rx_chans = 0;
 				memcpy(adm_params + sizeof(open_v8)
 						+ ep1_payload_size,
 						(void *)&ep2_payload,
