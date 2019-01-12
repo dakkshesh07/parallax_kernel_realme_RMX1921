768133e50e285fcc549a52248860e219723b9b84
Park Ju Hyung <qkrwngud825@gmail.com>
2019-01-13 06:50:17 +0900

    qcom-batt: reduce log spam
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/power/supply/qcom/battery.c
+++ drivers/power/supply/qcom/battery.c
@@ -612,7 +612,6 @@ static int pl_fcc_vote_callback(struct votable *votable, void *data,
 
 	if (!chip->main_psy)
 		return 0;
-
 	if (chip->pl_mode != POWER_SUPPLY_PL_NONE) {
 		get_fcc_split(chip, total_fcc_ua, &master_fcc_ua,
 				&slave_fcc_ua);
