97f92fc0a671e3b9a027a94679eb52c6af862596
Gustavo A. R. Silva <gustavo@embeddedor.com>
2019-01-23 02:14:13 -0600

    timers: Mark expected switch fall-throughs
    In preparation to enabling -Wimplicit-fallthrough, mark switch cases
    where fall through is indeed expected.
    
    Signed-off-by: Gustavo A. R. Silva <gustavo@embeddedor.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: John Stultz <john.stultz@linaro.org>
    Cc: Stephen Boyd <sboyd@kernel.org>
    Link: https://lkml.kernel.org/r/20190123081413.GA3949@embeddedor
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -358,7 +358,7 @@ static bool hrtimer_fixup_activate(void *addr, enum debug_obj_state state)
 	switch (state) {
 	case ODEBUG_STATE_ACTIVE:
 		WARN_ON(1);
-
+		fallthrough;
 	default:
 		return false;
 	}
--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -644,7 +644,7 @@ static bool timer_fixup_activate(void *addr, enum debug_obj_state state)
 
 	case ODEBUG_STATE_ACTIVE:
 		WARN_ON(1);
-
+		fallthrough;
 	default:
 		return false;
 	}
