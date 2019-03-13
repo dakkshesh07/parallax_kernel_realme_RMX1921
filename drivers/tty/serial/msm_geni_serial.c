892487cdb7006b493438bee93eb060bf6485a664
Park Ju Hyung <qkrwngud825@gmail.com>
2019-03-13 13:31:58 +0900

    msm_geni_serial: guard PM related logs with #ifdef DEBUG_PM
    This is useful but spams the kmsg too much
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -29,9 +29,8 @@
 #include <linux/tty.h>
 #include <linux/tty_flip.h>
 
-#ifdef VENDOR_EDIT //Cong.Dai@BSP.TP.Function, 2019/07/03, modified for replace daily build macro
-#include <soc/oppo/oppo_project.h>
-#endif /* VENDOR_EDIT */
+/* Show PM related logs */
+//#define DEBUG_PM
 
 /* UART specific GENI registers */
 #define SE_UART_LOOPBACK_CFG		(0x22C)
@@ -313,10 +312,13 @@ static int vote_clock_on(struct uart_port *uport)
 		return ret;
 	}
 	port->ioctl_count++;
+
+#ifdef DEBUG_PM
 	dev_info(uport->dev,
 		"pid: %d, comm: %s voted clock to be on, "
 		"ioctl_count: %d\n",
 			current->pid, current->comm, port->ioctl_count);
+#endif
 
 	return 0;
 }
@@ -341,10 +343,13 @@ static int vote_clock_off(struct uart_port *uport)
 				__func__, port->ioctl_count);
 		port->ioctl_count = 0;
 	}
+
+#ifdef DEBUG_PM
 	dev_info(uport->dev,
 		"pid: %d, comm: %s voted clock to be off, "
 		"ioctl_count: %d\n",
 			current->pid, current->comm, port->ioctl_count);
+#endif
 	msm_geni_serial_power_off(uport);
 
 	return 0;
@@ -1494,8 +1499,10 @@ static irqreturn_t msm_geni_wakeup_isr(int isr, void *dev)
 		port->edge_count = 0;
 		tty_flip_buffer_push(tty->port);
 		__pm_wakeup_event(&port->geni_wake, WAKEBYTE_TIMEOUT_MSEC);
+#ifdef DEBUG_PM
 		dev_info(uport->dev, "%s: Holding wakelock for %d ms\n",
 					__func__, WAKEBYTE_TIMEOUT_MSEC);
+#endif
 	} else if (port->edge_count < 2) {
 		port->edge_count++;
 	}
