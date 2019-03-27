be171f959244f74385d2ecceb3dfef0c20bf26bb
Sultan Alsawaf <sultan@kerneltoast.com>
2019-03-27 12:40:42 -0700

    clk: qcom: clk-cpu-osm: Don't sleep inside the global clk lock
    The clk framework uses a global mutex for all clock operations. This
    means that when one clock user has the lock held, no other clocks can be
    changed during that time. This can result in slow clock-rate propagation
    to unrelated clocks.
    
    To remedy this, only sleep within a per-cluster mutex lock. This not
    only allows different clusters to quickly update their clock rates
    independent of one another, but also prevents stalls when non-CPU clocks
    are updated, since the global clk lock will not be held for a long time.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- drivers/clk/qcom/clk-cpu-osm.c
+++ drivers/clk/qcom/clk-cpu-osm.c
@@ -210,8 +210,7 @@ static int clk_cpu_set_rate(struct clk_hw *hw, unsigned long rate,
 	struct clk_osm *c = to_clk_osm(hw);
 	struct clk_hw *p_hw = clk_hw_get_parent(hw);
 	struct clk_osm *parent = to_clk_osm(p_hw);
-	int core_num, current_index, index;
-	s64 delta_us;
+	int core_num, index;
 
 	if (!c || !parent)
 		return -EINVAL;
@@ -224,20 +223,6 @@ static int clk_cpu_set_rate(struct clk_hw *hw, unsigned long rate,
 	}
 
 	core_num = parent->per_core_dcvs ? c->core_num : 0;
-
-	/* Skip the update if the current rate is the same as the new one */
-	current_index = clk_osm_read_reg(parent,
-				DCVS_PERF_STATE_DESIRED_REG(core_num,
-							is_sdm845v1));
-	if (current_index == index)
-		return 0;
-
-	/* The old rate needs time to settle before it can be changed again */
-	delta_us = ktime_us_delta(ktime_get_boottime(), parent->last_update);
-	if (delta_us < 10000)
-		usleep_range(10000 - delta_us, 11000 - delta_us);
-	parent->last_update = ktime_get_boottime();
-
 	clk_osm_write_reg(parent, index,
 				DCVS_PERF_STATE_DESIRED_REG(core_num,
 							is_sdm845v1));
@@ -700,9 +685,8 @@ osm_set_index(struct clk_osm *c, unsigned int index)
 
 	/* The old rate needs time to settle before it can be changed again */
 	delta_us = ktime_us_delta(ktime_get_boottime(), parent->last_update);
-	if (delta_us < MIN_RATE_LIMIT_US)
-		usleep_range(MIN_RATE_LIMIT_US - delta_us,
-			     MAX_RATE_LIMIT_US - delta_us);
+	if (delta_us < 10000)
+		usleep_range(10000 - delta_us, 11000 - delta_us);
 	parent->last_update = ktime_get_boottime();
 
 	clk_set_rate(c->hw.clk, clk_round_rate(c->hw.clk, rate));
