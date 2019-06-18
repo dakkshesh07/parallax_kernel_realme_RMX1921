c032095b67707e8e2e3658c3346098d4a6ee5bd2
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2019-06-18 17:34:16 +0200

    drivers: base: power: remove wakeup_sources_stats_dentry variable
    wakeup_sources_stats_dentry is assigned when the debugfs file is
    created, but then never used ever again.  So no need for it at all, just
    remove it and call debugfs_create_file() on its own.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -1033,8 +1033,6 @@ void pm_wakep_autosleep_enabled(bool set)
 }
 #endif /* CONFIG_PM_AUTOSLEEP */
 
-static struct dentry *wakeup_sources_stats_dentry;
-
 /**
  * print_wakeup_source_stats - Print wakeup source statistics information.
  * @m: seq_file to print the statistics into.
@@ -1121,8 +1119,8 @@ static const struct file_operations wakeup_sources_stats_fops = {
 
 static int __init wakeup_sources_debugfs_init(void)
 {
-	wakeup_sources_stats_dentry = debugfs_create_file("wakeup_sources",
-			S_IRUGO, NULL, NULL, &wakeup_sources_stats_fops);
+	debugfs_create_file("wakeup_sources", S_IRUGO, NULL, NULL,
+			    &wakeup_sources_stats_fops);
 
     proc_create_data("wakeup_sources", 0444, NULL, &wakeup_sources_stats_fops, NULL);
     
