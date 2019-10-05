60456be79bfef4407c188da2ac64318bd8c6b887
Joe Perches <joe@perches.com>
2019-10-05 09:46:42 -0700

    BACKPORT: compiler_types.h: Add 'fallthrough' pseudo keyword for switch/case use
    Reserve the pseudo keyword 'fallthrough' for the ability to convert the
    various case block /* fallthrough */ style comments to appear to be an
    actual reserved word with the same gcc case block missing fallthrough
    warning capability.
    
    All switch/case blocks now should end in one of:
    
    	break;
    	fallthrough;
    	goto <label>;
    	return [expression];
    	continue;
    
    In C mode, GCC supports the __fallthrough__ attribute since 7.1,
    the same time the warning and the comment parsing were introduced.
    
    fallthrough devolves to an empty "do {} while (0)" if the compiler
    version (any version less than gcc 7) does not support the attribute.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Suggested-by: Dan Carpenter <dan.carpenter@oracle.com>
    Cc: Miguel Ojeda <miguel.ojeda.sandonis@gmail.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    [dakkshesh07: Move define from compiler_attributes.h -> compiler.h]
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -454,6 +454,23 @@ unsigned long read_word_at_a_time(const void *addr)
 # define __attribute_const__	/* unimplemented */
 #endif
 
+/*
+ * Add the pseudo keyword 'fallthrough' so case statement blocks
+ * must end with any of these keywords:
+ *   break;
+ *   fallthrough;
+ *   continue;
+ *   goto <label>;
+ *   return [expression];
+ *
+ *  gcc: https://gcc.gnu.org/onlinedocs/gcc/Statement-Attributes.html#Statement-Attributes
+ */
+#if __has_attribute(__fallthrough__)
+# define fallthrough                    __attribute__((__fallthrough__))
+#else
+# define fallthrough                    do {} while (0)  /* fallthrough */
+#endif
+
 #ifndef __latent_entropy
 # define __latent_entropy
 #endif
