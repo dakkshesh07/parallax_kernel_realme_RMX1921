f51bfe3b6ac2c6a22712c33c1b89c6cb7de7600a
Yaroslav Furman <yaro330@gmail.com>
2019-12-28 14:56:34 +0300

    blkdev: switch to SSD mode, disable IO stats and entropy gathering
    Optimize for flash storage.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -528,7 +528,7 @@ struct request_queue {
 
 #define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_STACKABLE)	|	\
 				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
-				 (1 << QUEUE_FLAG_ADD_RANDOM))
+				 (0 << QUEUE_FLAG_NONROT))
 
 #define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_STACKABLE)	|	\
 				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
