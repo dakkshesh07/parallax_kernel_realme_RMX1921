be7cc6e17ae9488f10c75c386d93af98e9da8886
Kailash <kailash.sudhakar@gmail.com>
2020-06-29 03:08:00 +0530

    sched: fair: Nuke tracing
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7120,8 +7120,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			target_cpu = -1;
 
 			fbt_env->fastpath = PREV_CPU_FASTPATH;
-			trace_sched_find_best_target(p, prefer_idle, min_util,
-					cpu, -1, -1, -1, target_cpu, -1, boosted);
 			goto out;
 		}
 	}
@@ -7485,10 +7483,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 		*backup_cpu = -1;
 	}
 out:
-	trace_sched_find_best_target(p, prefer_idle, min_util, cpu,
-				     best_idle_cpu, best_active_cpu,
-				     target_cpu, *backup_cpu);
-
 	schedstat_inc(p->se.statistics.nr_wakeups_fbt_count);
 	schedstat_inc(this_rq()->eas_stats.fbt_count);
 
