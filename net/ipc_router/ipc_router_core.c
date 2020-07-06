1494c87358981617d57ba08b0072d44c6a5a2bb2
pjgowtham <pjgowtham@gmail.com>
2020-07-06 09:15:52 +0530

    Revert "Add net/ modifications"
    This reverts commit 26ce925b21830f912cc2e5c887adb9135574e255.

--- net/ipc_router/ipc_router_core.c
+++ net/ipc_router/ipc_router_core.c
@@ -239,11 +239,7 @@ static int is_sensor_port(struct msm_ipc_router_remote_port *rport)
 
 	if (rport && rport->server) {
 		svcid = rport->server->name.service;
-		//#ifdef VENDOR_EDIT
-		//if (svcid == 400||(svcid >= 256 && svcid <= 320))
-		//Zengchao@PSW.BSP.Sensor 2018/8/11 add for to hold lock
-		if ((svcid >= 256 && svcid <= 320))
-		//#endif
+		if (svcid == 400 || (svcid >= 256 && svcid <= 320))
 			return true;
 	}
 
