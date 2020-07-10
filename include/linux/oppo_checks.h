573eac1fa92bba7e1a166b536ec4c2904cf71675
pjgowtham <pjgowtham@gmail.com>
2020-07-10 22:11:18 +0530

    Remove nfc checks for now
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- include/linux/oppo_checks.h
+++ include/linux/oppo_checks.h
@@ -1,7 +1,5 @@
 #ifndef __INCLUDE_LINUX_OPPO_CHECKS_H
 #define __INCLUDE_LINUX_OPPO_CHECKS_H
-static bool has_nfc;
 static bool on_ambient_screen;
 bool device_is_dozing(void);
-bool device_has_nfc(void);
 #endif 
