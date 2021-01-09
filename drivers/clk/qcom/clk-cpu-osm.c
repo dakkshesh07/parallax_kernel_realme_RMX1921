604c3ba90a4365deb2366b4e8a4469f1ac100dee
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-09 01:16:28 -0800

    clk: qcom: clk-cpu-osm: Set each CPU clock to its max when waking up
    The default frequency on Qualcomm CPUs is the lowest frequency supported
    by the CPU. This hurts latency when waking from suspend, as each CPU
    coming online runs at its lowest frequency until the governor can take
    over later. To speed up waking from suspend, hijack the CPUHP_AP_ONLINE
    hook and use it to set the highest available frequency on each CPU as
    they come online. This is done behind the governor's back but it's fine
    because the governor isn't running at this point in time for a CPU
    that's coming online.
    
    This speeds up waking from suspend significantly.
    
    dakkshesh07: Adapted for k4.9
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/clk/qcom/clk-cpu-osm.c
+++ drivers/clk/qcom/clk-cpu-osm.c
@@ -101,6 +101,13 @@ struct clk_osm {
 	struct mutex update_lock;
 };
 
+struct clk_osm_boost {
+	struct clk_osm *c;
+	unsigned int max_index;
+};
+
+static DEFINE_PER_CPU(struct clk_osm_boost, clk_boost_pcpu);
+
 static inline struct clk_osm *to_clk_osm(struct clk_hw *_hw)
 {
 	return container_of(_hw, struct clk_osm, hw);
@@ -746,7 +753,7 @@ static int osm_cpufreq_cpu_init(struct cpufreq_policy *policy)
 	struct clk_osm *c, *parent;
 	struct clk_hw *p_hw;
 	int ret, nr_opp, of_len;
-	unsigned int i, prev_cc = 0;
+	unsigned int i, prev_cc = 0, cpu;
 	unsigned int xo_kHz;
 	u32 *of_table = NULL;
 	char tbl_name[] = "qcom,cpufreq-table-##";
@@ -847,6 +854,10 @@ static int osm_cpufreq_cpu_init(struct cpufreq_policy *policy)
 
 	policy->cpuinfo.transition_latency = MIN_RATE_LIMIT_US;
 	policy->driver_data = c;
+	for_each_cpu(cpu, &c->related_cpus) {
+		per_cpu(clk_boost_pcpu, cpu).c = c;
+		per_cpu(clk_boost_pcpu, cpu).max_index = i - 1;
+	}
 
 	em_register_perf_domain(policy->cpus, nr_opp, &em_cb);
 
@@ -884,6 +895,16 @@ static struct cpufreq_driver qcom_osm_cpufreq_driver = {
 	.attr		= osm_cpufreq_attr,
 	.boost_enabled	= true,
 };
+
+static int cpuhp_osm_online(unsigned int cpu)
+{
+	struct clk_osm_boost *b = &per_cpu(clk_boost_pcpu, cpu);
+	struct clk_osm *c = b->c;
+
+	/* Set the max frequency by default before the governor takes over */
+	osm_set_index(c, b->max_index);
+	return 0;
+}
 
 static u32 find_voltage(struct clk_osm *c, unsigned long rate)
 {
@@ -1413,6 +1434,11 @@ static int clk_cpu_osm_driver_probe(struct platform_device *pdev)
 	rc = cpufreq_register_driver(&qcom_osm_cpufreq_driver);
 	if (rc)
 		goto provider_err;
+
+	rc = cpuhp_setup_state_nocalls(CPUHP_AP_ONLINE, "osm-cpufreq:online",
+				       cpuhp_osm_online, NULL);
+	if (rc)
+		dev_err(&pdev->dev, "CPUHP callback setup failed, rc=%d\n", rc);
 
 	pr_info("OSM CPUFreq driver inited\n");
 	return 0;
