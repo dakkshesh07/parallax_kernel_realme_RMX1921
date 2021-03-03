2f26edaaa18143389da7a264eeaae2376df3bd2d
kaderbava <ksbava7325@gmail.com>
2021-03-03 21:23:44 +0000

    Merge branch 'android-4.9-q' of https://android.googlesource.com/kernel/common into caf-10
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -2362,6 +2362,7 @@ arch_get_unmapped_area(struct file *filp, unsigned long addr,
 	info.low_limit = mm->mmap_base;
 	info.high_limit = TASK_SIZE;
 	info.align_mask = 0;
+	info.align_offset = 0;
 	return vm_unmapped_area(&info);
 }
 #endif
@@ -2417,6 +2418,7 @@ arch_get_unmapped_area_topdown(struct file *filp, const unsigned long addr0,
 #else
 	info.high_limit = mm->mmap_base;
 	info.align_mask = 0;
+	info.align_offset = 0;
 #endif
 	addr = vm_unmapped_area(&info);
 
@@ -3640,6 +3642,7 @@ void exit_mmap(struct mm_struct *mm)
 		if (vma->vm_flags & VM_ACCOUNT)
 			nr_accounted += vma_pages(vma);
 		vma = remove_vma(vma);
+		cond_resched();
 	}
 	vm_unacct_memory(nr_accounted);
 }
