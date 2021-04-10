65c8b7af4aa69ae2459a5a9173885db145fb6941
Panchajanya1999 <panchajanya@azure-dev.live>
2021-04-10 20:25:08 +0530

    msm: vidc: Disable logging on !DEBUG_KERNEL
    Keeps kernel logs clean from useless vidc debug spams.
    
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- drivers/media/platform/msm/vidc/msm_vidc.c
+++ drivers/media/platform/msm/vidc/msm_vidc.c
@@ -1742,9 +1742,10 @@ void *msm_vidc_open(int core_id, int session_type)
 		rc = -ENOMEM;
 		goto err_invalid_core;
 	}
-
+#ifdef CONFIG_DEBUG_KERNEL
 	pr_debug(VIDC_DBG_TAG "Opening video instance: %pK, %d\n",
 		VIDC_MSG_PRIO2STRING(VIDC_INFO), inst, session_type);
+#endif
 	mutex_init(&inst->sync_lock);
 	mutex_init(&inst->bufq[CAPTURE_PORT].lock);
 	mutex_init(&inst->bufq[OUTPUT_PORT].lock);
@@ -1991,9 +1992,10 @@ int msm_vidc_destroy(struct msm_vidc_inst *inst)
 	mutex_destroy(&inst->flush_lock);
 
 	msm_vidc_debugfs_deinit_inst(inst);
-
+#ifdef CONFIG_DEBUG_KERNEL
 	pr_debug(VIDC_DBG_TAG "Closed video instance: %pK\n",
 			VIDC_MSG_PRIO2STRING(VIDC_INFO), inst);
+#endif
 	kfree(inst);
 	return 0;
 }
