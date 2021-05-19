b10a514ba6b859f34991d4abda9b37a46dbd2fd6
Dakkshesh <dakkshesh5@gmail.com>
2021-05-19 20:48:28 +0530

    Rebrand kernel

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,35 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Parallax Kernel For Realme XT
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=RMX1921
+device.name2=RMX1921EU
+supported.versions=11
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel install
+dump_boot;
+
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
+
+write_boot;
+## end install
---
+++ build.sh
@@ -0,0 +1,106 @@
+clear
+
+# Clean build always lol
+echo "**** Cleaning ****"
+rm -r out
+mkdir out
+make O=out clean
+make clean && make distclean && make mrproper
+
+# auto clang cloner/updater
+echo "**** Cleaning ****"
+if [ -r clang ]; then
+	echo "old clang found! would u like to clone again latest clang? ( Y/N )"
+
+	read answer
+	if [[ $answer == 'Y' ]]; then
+		echo "OK, cleaning old clang files"
+		rm -rf clang
+		git clone https://github.com/kdrag0n/proton-clang.git --depth=1 clang
+	elif [[ $answer == 'y' ]]; then
+		echo "OK, cleaning old clang files"
+		rm -rf clang
+		git clone https://github.com/kdrag0n/proton-clang.git --depth=1 clang
+	elif [[ $answer == 'N' ]]; then
+		echo "OK, continuing with compilation"
+	elif [[ $answer == 'n' ]]; then
+		echo "OK, continuing compilation"
+	fi
+
+else
+  echo "clang not found!, git cloning it now...."
+  git clone https://github.com/kdrag0n/proton-clang.git --depth=1 clang
+
+fi
+
+
+KERNEL_DEFCONFIG=RMX1921_defconfig
+Device="MI 9SE (GRUS)"
+ANYKERNEL3_DIR=$PWD/AnyKernel3/
+KERNELDIR=$PWD/
+FINAL_KERNEL_ZIP=parallax_v1.zip
+export PATH="${PWD}/clang/bin:${PATH}"
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_COMPILER_STRING="$(${PWD}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+# Speed up build process
+MAKE="./makeparallel"
+
+BUILD_START=$(date +"%s")
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+green='\033[0;32m'
+nocol='\033[0m'
+
+echo ""
+echo -e "$blue***********************************************"
+echo "**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****"
+echo -e "***********************************************$nocol"
+echo "--------------------------------------------------------"
+echo -e "$blue***********************************************"
+echo "            BUILDING KERNEL          "
+echo -e "***********************************************$nocol"
+echo ""
+make $KERNEL_DEFCONFIG O=out
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                      NM=llvm-nm \
+                      OBJCOPY=llvm-objcopy \
+                      OBJDUMP=llvm-objdump \
+                      STRIP=llvm-strip
+
+echo "**** Verify Image.gz-dtb ****"
+if [ -f out/arch/arm64/boot/Image.gz-dtb ]; then
+  echo "**** Removing leftovers ****"
+  rm -rf $ANYKERNEL3_DIR/Image.gz-dtb
+  rm -rf $ANYKERNEL3_DIR/dtbo.img
+  rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
+
+  echo "**** Copying Image.gz-dtb ****"
+  cp $PWD/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL3_DIR/
+  cp $PWD/out/arch/arm64/boot/dtbo.img $ANYKERNEL3_DIR/
+
+  echo "**** Time to zip up! ****"
+  cd $ANYKERNEL3_DIR/
+  zip -r9 $FINAL_KERNEL_ZIP * -x README $FINAL_KERNEL_ZIP
+  cp $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP $KERNELDIR/$FINAL_KERNEL_ZIP
+  echo "**** Done, here is your build info ****"
+
+  BUILD_END=$(date +"%s")
+  DIFF=$(($BUILD_END - $BUILD_START))
+  echo -e "$yellow Device:-$Device.$nocol"
+  echo -e "$yellow Build Time:- Completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
+
+else
+  echo "**** Build Failed ****"
+  echo "**** Removing leftovers ****"
+  rm -rf out/
+  BUILD_END=$(date +"%s")
+  DIFF=$(($BUILD_END - $BUILD_START))
+  echo -e "$red mission failed we'll get em next time, Build failed in:- $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
+fi
