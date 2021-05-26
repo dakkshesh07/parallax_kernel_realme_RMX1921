b089d07ecd5b66302d8c392e168efd5b5ad2463a
Dakkshesh <dakkshesh5@gmail.com>
2021-05-26 13:33:44 +0530

    Revert "mm/vmscan.c: swappiness set to 45"
    This reverts commit cdede3e4e89bcdf480e22e2acc4c6e0629aecada.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -164,7 +164,15 @@ struct scan_control {
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 45;
+int vm_swappiness = 60;
+
+#ifdef VENDOR_EDIT //yixue.ge@psw.bsp.kernel 20170720 add for add direct_vm_swappiness
+/*
+ * Direct reclaim swappiness, exptct 0 - 60. Higher means more swappy and slower.
+ */
+int direct_vm_swappiness = 60;
+#endif
+
 /*
  * The total number of pages which are beyond the high watermark within all
  * zones.
