febbeda5c8b45e129763751759b977b8ace71129
Deepa Dinamani <deepa.kernel@gmail.com>
2021-06-16 23:10:43 +0530

    time: Add new y2038 safe __kernel_timespec
    The new struct __kernel_timespec is similar to current
    internal kernel struct timespec64 on 64 bit architecture.
    The compat structure however is similar to below on little
    endian systems (padding and tv_nsec are switched for big
    endian systems):
    
    typedef s32            compat_long_t;
    typedef s64            compat_kernel_time64_t;
    
    struct compat_kernel_timespec {
           compat_kernel_time64_t  tv_sec;
           compat_long_t           tv_nsec;
           compat_long_t           padding;
    };
    
    This allows for both the native and compat representations to
    be the same and syscalls using this type as part of their ABI
    can have a single entry point to both.
    
    Note that the compat define is not included anywhere in the
    kernel explicitly to avoid confusion.
    
    These types will be used by the new syscalls that will be
    introduced in the consequent patches.
    Most of the new syscalls are just an update to the existing
    native ones with this new type. Hence, put this new type under
    an ifdef so that the architectures can define CONFIG_64BIT_TIME
    when they are ready to handle this switch.
    
    Cc: linux-arch@vger.kernel.org
    Signed-off-by: Deepa Dinamani <deepa.kernel@gmail.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- include/linux/time64.h
+++ include/linux/time64.h
@@ -1,12 +1,20 @@
 #ifndef _LINUX_TIME64_H
 #define _LINUX_TIME64_H
 
-#include <uapi/linux/time.h>
 #include <linux/math64.h>
 #include <vdso/time64.h>
 
 typedef __s64 time64_t;
 typedef __u64 timeu64_t;
+
+/* CONFIG_64BIT_TIME enables new 64 bit time_t syscalls in the compat path
+ * and 32-bit emulation.
+ */
+#ifndef CONFIG_64BIT_TIME
+#define __kernel_timespec timespec
+#endif
+
+#include <uapi/linux/time.h>
 
 /*
  * This wants to go into uapi/linux/time.h once we agreed about the
