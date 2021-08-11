5065a97622b5cde3434c32c9737e55b3a0e7c71f
Dakkshesh <dakkshesh5@gmail.com>
2021-08-11 12:50:01 +0530

    treewide: make gcc happy again
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- techpack/audio/asoc/codecs/tfa98xx/tfa98xx.c
+++ techpack/audio/asoc/codecs/tfa98xx/tfa98xx.c
@@ -1000,7 +1000,7 @@ static ssize_t tfa98xx_dbgfs_dsp_state_set(struct file *file,
     struct i2c_client *i2c = file->private_data;
     struct tfa98xx *tfa98xx = i2c_get_clientdata(i2c);
     enum tfa_error ret;
-    char buf[32];
+    char buf[32] ="";
     const char start_cmd[] = "start";
     const char stop_cmd[] = "stop";
 	#ifndef VENDOR_EDIT
