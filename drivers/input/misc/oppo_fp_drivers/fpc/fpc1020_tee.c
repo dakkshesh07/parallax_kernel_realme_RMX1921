a8142fee5bbda699bfa0d507ced7663ba4a389d4
Dakkshesh <dakkshesh5@gmail.com>
2021-08-23 10:35:23 +0530

    input: oppo_fp_drivers: Mark IRQ as performance critical
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/input/misc/oppo_fp_drivers/fpc/fpc1020_tee.c
+++ drivers/input/misc/oppo_fp_drivers/fpc/fpc1020_tee.c
@@ -429,7 +429,7 @@ static int fpc1020_probe(struct platform_device *pdev)
 
         /*dev_info(fpc1020->dev, "fpc1020 requested gpio finished \n");*/
 
-        irqf = IRQF_TRIGGER_RISING | IRQF_ONESHOT;
+        irqf = IRQF_TRIGGER_RISING | IRQF_ONESHOT | IRQF_PERF_CRITICAL;
         mutex_init(&fpc1020->lock);
         rc = devm_request_threaded_irq(dev, gpio_to_irq(fpc1020->irq_gpio),
                         NULL, fpc1020_irq_handler, irqf,
