807253ae5083221c3b06ced676b909d705a497ba
Dakkshesh <dakkshesh5@gmail.com>
2021-08-31 20:03:15 +0530

    oppo_touchscreen: switch to msm drm notifier
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/input/touchscreen/oppo_touchscreen/touchpanel_common.h
+++ drivers/input/touchscreen/oppo_touchscreen/touchpanel_common.h
@@ -592,8 +592,8 @@ struct touchpanel_data {
 #if defined(TPD_USE_EINT)
     struct hrtimer         timer;                       /*using polling instead of IRQ*/
 #endif
-#if defined(CONFIG_FB)
-    struct notifier_block fb_notif;                     /*register to control suspend/resume*/
+#if defined(CONFIG_DRM_MSM)
+    struct notifier_block msm_drm_notif;                     /*register to control suspend/resume*/
 #endif
     struct monitor_data    monitor_data;
     struct mutex           mutex;                       /*mutex for lock i2c related flow*/
