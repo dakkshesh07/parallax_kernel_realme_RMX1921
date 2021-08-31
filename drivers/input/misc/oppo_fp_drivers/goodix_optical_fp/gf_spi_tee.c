b5458c9f62e738afe27db35589f3b8de68b267bd
Dakkshesh <dakkshesh5@gmail.com>
2021-08-31 23:03:38 +0530

    drivers: oppo_fp_drivers: Switch to msm drm notifier
    ok so now oppo devs write logs to make FB notifier look like drm notifier. well done!
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/input/misc/oppo_fp_drivers/goodix_optical_fp/gf_spi_tee.c
+++ drivers/input/misc/oppo_fp_drivers/goodix_optical_fp/gf_spi_tee.c
@@ -43,7 +43,7 @@
 #include <linux/of_gpio.h>
 #include <linux/timer.h>
 #include <linux/notifier.h>
-#include <linux/fb.h>
+#include <linux/msm_drm_notify.h>
 #include <linux/pm_qos.h>
 #include <linux/cpufreq.h>
 //#include <linux/wakelock.h>
@@ -724,13 +724,13 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 		unsigned long val, void *data)
 {
 	struct gf_dev *gf_dev;
-	struct fb_event *evdata = data;
+	struct msm_drm_notifier *evdata = data;
 	unsigned int blank;
 	char msg = 0;
-    int retval = 0;
+    	int retval = 0;
 
-	//if (val != FB_EARLY_EVENT_BLANK)
-	//	return 0;
+	if (val != MSM_DRM_EARLY_EVENT_BLANK)
+		return 0;
 	pr_info("[info] %s go to the goodix_fb_state_chg_callback value = %d\n",
 			__func__, (int)val);
 	gf_dev = container_of(nb, struct gf_dev, notifier);
@@ -755,10 +755,10 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
         return retval;
     }
 
-	if (evdata && evdata->data && val == FB_EARLY_EVENT_BLANK && gf_dev) {
+	if (evdata && evdata->data && val == MSM_DRM_EARLY_EVENT_BLANK && gf_dev) {
 		blank = *(int *)(evdata->data);
 		switch (blank) {
-		case FB_BLANK_POWERDOWN:
+		case MSM_DRM_BLANK_POWERDOWN:
 			if (gf_dev->device_available == 1) {
 				gf_dev->fb_black = 1;
 #if defined(GF_NETLINK_ENABLE)
@@ -771,7 +771,7 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 #endif
 			}
 			break;
-		case FB_BLANK_UNBLANK:
+		case MSM_DRM_BLANK_UNBLANK:
 			if (gf_dev->device_available == 1) {
 				gf_dev->fb_black = 0;
 #if defined(GF_NETLINK_ENABLE)
@@ -904,7 +904,7 @@ static int gf_probe(struct platform_device *pdev)
 #endif
 
 	gf_dev->notifier = goodix_noti_block;
-	fb_register_client(&gf_dev->notifier);
+	msm_drm_register_client(&gf_dev->notifier);
 
 	wake_lock_init(&fp_wakelock, WAKE_LOCK_SUSPEND, "fp_wakelock");
 	wake_lock_init(&gf_cmd_wakelock, WAKE_LOCK_SUSPEND, "gf_cmd_wakelock");
@@ -967,7 +967,7 @@ static int gf_remove(struct platform_device *pdev)
 		gf_cleanup(gf_dev);
 
 
-	fb_unregister_client(&gf_dev->notifier);
+	msm_drm_register_client(&gf_dev->notifier);
 	mutex_unlock(&device_list_lock);
 
 	return 0;
