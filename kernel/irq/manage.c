ca13de19a89074b6bc127121af355220afb4b405
Dakkshesh <dakkshesh5@gmail.com>
2021-09-17 08:53:06 +0530

    Revert "kernel: irq: manage: use a different way of affining perf IRQs"
    This reverts commit 2d45d6edc5c060d17991542bac120f38cc7aa756.
    
    Revert "kernel: Don't allow IRQ affinity masks to have more than one CPU"
    
    This reverts commit 9e2ce236462d94ce4103142018b563ce37b04730.
    
    Revert "kernel: Improve performance critical IRQ framework"
    
    This reverts commit 67db7b4614ac0d1ffc607c80dd42cea12eb85639.
    
    Revert "kernel: Add API to mark IRQs and kthreads as performance critical"
    
    This reverts commit 64121a49899da8b6d190e8d44ea951b32d9f0762.

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -18,18 +18,8 @@
 #include <linux/sched.h>
 #include <linux/sched/rt.h>
 #include <linux/task_work.h>
-#include <linux/cpu.h>
 
 #include "internals.h"
-
-struct irq_desc_list {
-	struct list_head list;
-	struct irq_desc *desc;
-} perf_crit_irqs = {
-	.list = LIST_HEAD_INIT(perf_crit_irqs.list)
-};
-static DEFINE_RAW_SPINLOCK(perf_irqs_lock);
-static int perf_cpu_index = -1;
 
 #ifdef CONFIG_IRQ_FORCED_THREADING
 __read_mostly bool force_irqthreads;
@@ -211,8 +201,6 @@ int irq_do_set_affinity(struct irq_data *data, const struct cpumask *mask,
 	struct irq_chip *chip = irq_data_get_irq_chip(data);
 	int ret;
 
-	/* IRQs only run on the first CPU in the affinity mask; reflect that */
-	mask = cpumask_of(cpumask_first(mask));
 	ret = chip->irq_set_affinity(data, mask, force);
 	switch (ret) {
 	case IRQ_SET_MASK_OK:
@@ -387,10 +375,6 @@ static int setup_affinity(struct irq_desc *desc, struct cpumask *mask)
 	}
 
 	cpumask_and(mask, cpu_online_mask, set);
-
-	if (irqd_has_set(&desc->irq_data, IRQF_PERF_CRITICAL))
-		cpumask_copy(mask, cpu_perf_mask);
-
 	if (node != NUMA_NO_NODE) {
 		const struct cpumask *nodemask = cpumask_of_node(node);
 
@@ -1150,86 +1134,6 @@ setup_irq_thread(struct irqaction *new, unsigned int irq, bool secondary)
 	return 0;
 }
 
-static void add_desc_to_perf_list(struct irq_desc *desc)
-{
-	struct irq_desc_list *item;
-
-	item = kmalloc(sizeof(*item), GFP_ATOMIC | __GFP_NOFAIL);
-	item->desc = desc;
-
-	raw_spin_lock(&perf_irqs_lock);
-	list_add(&item->list, &perf_crit_irqs.list);
-	raw_spin_unlock(&perf_irqs_lock);
-}
-
-static void affine_one_perf_thread(struct task_struct *t)
-{
-	t->flags |= PF_PERF_CRITICAL;
-	set_cpus_allowed_ptr(t, cpu_perf_mask);
-}
-
-static void unaffine_one_perf_thread(struct task_struct *t)
-{
-	t->flags &= ~PF_PERF_CRITICAL;
-	set_cpus_allowed_ptr(t, cpu_all_mask);
-}
-
-static void affine_one_perf_irq(struct irq_desc *desc)
-{
-	int cpu;
-
-	/* Balance the performance-critical IRQs across all perf CPUs */
-	get_online_cpus();
-	while (1) {
-		cpu = cpumask_next_and(perf_cpu_index, cpu_perf_mask,
-				       cpu_online_mask);
-		if (cpu < nr_cpu_ids)
-			break;
-		perf_cpu_index = -1;
-	}
-	irq_set_affinity_locked(&desc->irq_data, cpumask_of(cpu), true);
-	put_online_cpus();
-
-	perf_cpu_index = cpu;
-}
-
-void unaffine_perf_irqs(void)
-{
-	struct irq_desc_list *data;
-	unsigned long flags;
-
-	raw_spin_lock_irqsave(&perf_irqs_lock, flags);
-	list_for_each_entry(data, &perf_crit_irqs.list, list) {
-		struct irq_desc *desc = data->desc;
-
-		raw_spin_lock(&desc->lock);
-		irq_set_affinity_locked(&desc->irq_data, cpu_all_mask, true);
-		if (desc->action->thread)
-			unaffine_one_perf_thread(desc->action->thread);
-		raw_spin_unlock(&desc->lock);
-	}
-	perf_cpu_index = -1;
-	raw_spin_unlock_irqrestore(&perf_irqs_lock, flags);
-}
-
-void reaffine_perf_irqs(void)
-{
-	struct irq_desc_list *data;
-	unsigned long flags;
-
-	raw_spin_lock_irqsave(&perf_irqs_lock, flags);
-	list_for_each_entry(data, &perf_crit_irqs.list, list) {
-		struct irq_desc *desc = data->desc;
-
-		raw_spin_lock(&desc->lock);
-		affine_one_perf_irq(desc);
-		if (desc->action->thread)
-			affine_one_perf_thread(desc->action->thread);
-		raw_spin_unlock(&desc->lock);
-	}
-	raw_spin_unlock_irqrestore(&perf_irqs_lock, flags);
-}
-
 /*
  * Internal function to register an irqaction - typically used to
  * allocate special interrupts that are part of the architecture.
@@ -1459,15 +1363,7 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 		}
 
 		/* Set default affinity mask once everything is setup */
-		if (new->flags & IRQF_PERF_CRITICAL) {
-			add_desc_to_perf_list(desc);
-			irqd_set(&desc->irq_data, IRQD_AFFINITY_MANAGED);
-			raw_spin_lock(&perf_irqs_lock);
-			affine_one_perf_irq(desc);
-			raw_spin_unlock(&perf_irqs_lock);
-		} else {
-			setup_affinity(desc, mask);
-		}
+		setup_affinity(desc, mask);
 
 	} else if (new->flags & IRQF_TRIGGER_MASK) {
 		unsigned int nmsk = new->flags & IRQF_TRIGGER_MASK;
