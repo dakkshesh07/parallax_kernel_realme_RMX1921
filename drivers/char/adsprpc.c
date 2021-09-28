8e415d3e27043ab56f04256a10ab23d964852166
Forenche <61968330+Forenche@users.noreply.github.com>
2021-09-28 21:17:47 +0530

    char: adsprpc: Replace cpumasks with atomic bitwise operations
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -3915,7 +3915,6 @@ static int fastrpc_internal_control(struct fastrpc_file *fl,
 	int err = 0;
 	int latency;
 	cpumask_t mask;
-	struct fastrpc_apps *me = &gfa;
 	u32 len = me->silvercores.corecount, i = 0;
 
 	VERIFY(err, !IS_ERR_OR_NULL(fl) && !IS_ERR_OR_NULL(fl->apps));
@@ -3937,7 +3936,7 @@ static int fastrpc_internal_control(struct fastrpc_file *fl,
 		for (i = 0; i < len; i++)
 			cpumask_set_cpu(me->silvercores.coreno[i], &mask);
 		fl->pm_qos_req.type = PM_QOS_REQ_AFFINE_CORES;
-		cpumask_copy(&fl->pm_qos_req.cpus_affine, &mask);
+		atomic_set(&fl->pm_qos_req.cpus_affine, *cpumask_bits(&mask));
 
 		mutex_lock(&fl->pm_qos_mutex);
 		if (!fl->qos_request) {
