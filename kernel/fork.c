080c3d405d5a2c99695d55f09bf692c5684e1fb5
Dakkshesh <dakkshesh5@gmail.com>
2021-09-29 23:44:49 +0530

    kernel: fork: Adjust zygote boost duration according to KProfile level
    level 1 (battery) = no boost
    level 2 (balance) = 25ms boost
    level 0 (default) / level 3 (gaming) = 50ms boost
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -83,6 +83,7 @@
 #include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
 #include <linux/simple_lmk.h>
+#include <linux/kprofiles.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2108,9 +2109,18 @@ long _do_fork(unsigned long clone_flags,
 
 	/* Boost CPU to the max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current)) {
-		cpu_input_boost_kick_max(50);
-		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 50);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
+	/* Dont boost if battery saver profile is enabled */
+		if (active_mode() == 3 || active_mode() == 0) {
+			cpu_input_boost_kick_max(50);
+			devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 50);
+			devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
+		} else if (active_mode() == 2) {
+			cpu_input_boost_kick_max(25);
+			devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 25);
+			devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 25);
+		} else {
+			pr_info("Battery Profile Active, Skipping Boosting...\n");
+		}
 	}
 
 	/*
