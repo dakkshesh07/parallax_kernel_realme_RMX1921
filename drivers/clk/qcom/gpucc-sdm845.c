ff1822d4ac67e6c97082ec4e4fdc7b5bcd3757de
Dakkshesh <dakkshesh5@gmail.com>
2021-11-17 22:07:06 +0530

    drivers: clk: gpucc-sdm845: use VDD_GX_MIN for 267mhz
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -647,7 +647,7 @@ static void gpu_cc_gfx_sdm845_fixup_sdm670(void)
 	gpu_cc_gx_gfx3d_clk_src.freq_tbl =
 				ftbl_gpu_cc_gx_gfx3d_clk_src_sdm670;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_MIN] = 180000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_LOWER] =
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_MIN] =
 		267000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_LOW] = 355000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_LOW_L1] =
