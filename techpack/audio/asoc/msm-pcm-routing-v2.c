2ecfbb332d53373960962ddcea9141e89fc54fef
Dakkshesh <dakkshesh5@gmail.com>
2021-12-30 10:56:53 +0530

    techpack: msm-pcm-routing-v2: Silence Sound Source Tracking logspam
    Silences:
    
    [   21.056106] (6)[840:audio.service]msm_audio_get_copp_idx_from_port_id: Invalid FE, exiting
    [   21.056108] (6)[840:audio.service]msm_audio_sound_focus_get: Could not get copp idx for port_id=16385
    [   21.056213] (6)[840:audio.service]msm_audio_get_copp_idx_from_port_id: Invalid FE, exiting
    [   21.056216] (6)[840:audio.service]msm_audio_source_tracking_get: Could not get copp idx for port_id=16385
    [   21.056322] (6)[840:audio.service]msm_voice_sound_focus_get: Error getting Sound Focus Params, err=-22
    [   21.056421] (6)[840:audio.service]msm_voice_source_tracking_get: Error getting Source Tracking Params, err=-22
    [   21.056517] (6)[840:audio.service]msm_audio_get_copp_idx_from_port_id: Invalid FE, exiting
    [   21.056519] (6)[840:audio.service]msm_audio_sound_focus_get: Could not get copp idx for port_id=4101
    [   21.056614] (6)[840:audio.service]msm_audio_get_copp_idx_from_port_id: Invalid FE, exiting
    [   21.056616] (6)[840:audio.service]msm_audio_source_tracking_get: Could not get copp idx for port_id=4101
    [   21.056712] (6)[840:audio.service]msm_voice_sound_focus_get: Error getting Sound Focus Params, err=-22
    [   21.056808] (6)[840:audio.service]msm_voice_source_tracking_get: Error getting Source Tracking Params, err=-22
    [   21.056904] (6)[840:audio.service]msm_audio_get_copp_idx_from_port_id: Invalid FE, exiting
    [   21.056906] (6)[840:audio.service]msm_audio_sound_focus_get: Could not get copp idx for port_id=4149
    [   21.057000] (6)[840:audio.service]msm_audio_get_copp_idx_from_port_id: Invalid FE, exiting
    [   21.057002] (6)[840:audio.service]msm_audio_source_tracking_get: Could not get copp idx for port_id=4149
    [   21.057096] (6)[840:audio.service]msm_audio_get_copp_idx_from_port_id: Invalid FE, exiting
    [   21.057098] (6)[840:audio.service]msm_audio_sound_focus_get: Could not get copp idx for port_id=36865
    [   21.057190] (6)[840:audio.service]msm_audio_get_copp_idx_from_port_id: Invalid FE, exiting
    [   21.057192] (6)[840:audio.service]msm_audio_source_tracking_get: Could not get copp idx for port_id=36865
    ...
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- techpack/audio/asoc/msm-pcm-routing-v2.c
+++ techpack/audio/asoc/msm-pcm-routing-v2.c
@@ -15671,7 +15671,7 @@ static int msm_audio_get_copp_idx_from_port_id(int port_id, int session_type,
 			break;
 	}
 	if (i >= MSM_FRONTEND_DAI_MM_SIZE) {
-		pr_err("%s: Invalid FE, exiting\n", __func__);
+		pr_debug("%s: Invalid FE, exiting\n", __func__);
 
 		ret = -EINVAL;
 		goto done;
@@ -15741,7 +15741,7 @@ static int msm_audio_sound_focus_put(struct snd_kcontrol *kcontrol,
 	ret = msm_audio_get_copp_idx_from_port_id(port_id, SESSION_TYPE_TX,
 					    &copp_idx);
 	if (ret) {
-		pr_err("%s: Could not get copp idx for port_id=%d\n",
+		pr_debug("%s: Could not get copp idx for port_id=%d\n",
 			__func__, port_id);
 
 		ret = -EINVAL;
