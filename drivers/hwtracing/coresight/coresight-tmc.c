74433acec90645c13adf284ac7c78496b5bdd3df
Dakkshesh <dakkshesh5@gmail.com>
2022-02-04 13:47:33 +0530

    drivers: coresight: Fix some potential buffer overflows
    Clang warns:
    ../drivers/hwtracing/coresight/coresight-tmc.c:427:26: warning: 'sscanf' may overflow; destination buffer in argument 3 has size 10, but the corresponding specifier may require size 11 [-Wfortify-source]
            if (sscanf(buf, "%10s", str) != 1)
                                    ^
    ../drivers/hwtracing/coresight/coresight-tmc.c:529:26: warning: 'sscanf' may overflow; destination buffer in argument 3 has size 10, but the corresponding specifier may require size 11 [-Wfortify-source]
            if (sscanf(buf, "%10s", str) != 1)
                                    ^
    2 warnings generated.
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/hwtracing/coresight/coresight-tmc.c
+++ drivers/hwtracing/coresight/coresight-tmc.c
@@ -418,7 +418,7 @@ static ssize_t out_mode_store(struct device *dev,
 			      const char *buf, size_t size)
 {
 	struct tmc_drvdata *drvdata = dev_get_drvdata(dev->parent);
-	char str[10] = "";
+	char str[11] = "";
 	unsigned long flags;
 	int ret;
 
@@ -522,7 +522,7 @@ static ssize_t mem_type_store(struct device *dev,
 			      size_t size)
 {
 	struct tmc_drvdata *drvdata = dev_get_drvdata(dev->parent);
-	char str[10] = "";
+	char str[11] = "";
 
 	if (strlen(buf) >= 10)
 		return -EINVAL;
