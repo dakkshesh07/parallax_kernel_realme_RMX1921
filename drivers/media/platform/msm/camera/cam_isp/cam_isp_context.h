c80a86cd4cae937ec3737e34254c4df3d4873000
Dakkshesh <dakkshesh5@gmail.com>
2022-06-15 19:13:13 +0530

    driver/media: msm: camera: import realme changes
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/media/platform/msm/camera/cam_isp/cam_isp_context.h
+++ drivers/media/platform/msm/camera/cam_isp/cam_isp_context.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017-2020, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2017-2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -33,11 +33,6 @@
  */
 #define CAM_ISP_CTX_CFG_MAX                     22
 
-/* Maximum allowed sof count in rdi only bubble state
- * till buf_done is received for bubble req_id.
- */
-#define CAM_ISP_CTX_BUBBLE_SOF_COUNT_MAX        3
-
 /*
  * Maximum entries in state monitoring array for error logging
  */
@@ -62,6 +57,7 @@ enum cam_isp_ctx_activated_substate {
 	CAM_ISP_CTX_ACTIVATED_BUBBLE_APPLIED,
 	CAM_ISP_CTX_ACTIVATED_HW_ERROR,
 	CAM_ISP_CTX_ACTIVATED_HALT,
+	CAM_ISP_CTX_ACTIVATED_FLUSH,
 	CAM_ISP_CTX_ACTIVATED_MAX,
 };
 
@@ -120,7 +116,6 @@ struct cam_isp_ctx_req {
 	uint32_t                              num_acked;
 	int32_t                               bubble_report;
 	struct cam_isp_prepare_hw_update_data hw_update_data;
-	bool                                  bubble_detected;
 };
 
 /**
@@ -148,8 +143,6 @@ struct cam_isp_context_state_monitor {
  * @base:                      Common context object pointer
  * @frame_id:                  Frame id tracking for the isp context
  * @substate_actiavted:        Current substate for the activated state.
- * @process_bubble:            Atomic variable to check if ctx is still
- *                             processing bubble
  * @substate_machine:          ISP substate machine for external interface
  * @substate_machine_irq:      ISP substate machine for irq handling
  * @req_base:                  Common request object storage
@@ -162,12 +155,11 @@ struct cam_isp_context_state_monitor {
  * @subscribe_event:           The irq event mask that CRM subscribes to, IFE
  *                             will invoke CRM cb at those event.
  * @last_applied_req_id:       Last applied request id
+ * @frame_skip_count:          Number of frame to skip before change state
  * @state_monitor_head:        Write index to the state monitoring array
  * @cam_isp_ctx_state_monitor: State monitoring array
  * @rdi_only_context:          Get context type information.
  *                             true, if context is rdi only context
- * @bubble_sof_count:          Atomic variable to check if ctx has any sof's
- *                             while processing bubble
  *
  */
 struct cam_isp_context {
@@ -175,7 +167,6 @@ struct cam_isp_context {
 
 	int64_t                          frame_id;
 	uint32_t                         substate_activated;
-	atomic_t                         process_bubble;
 	struct cam_ctx_ops              *substate_machine;
 	struct cam_isp_ctx_irq_ops      *substate_machine_irq;
 
@@ -189,11 +180,11 @@ struct cam_isp_context {
 	int64_t                          reported_req_id;
 	uint32_t                         subscribe_event;
 	int64_t                          last_applied_req_id;
+	uint32_t                         frame_skip_count;
 	atomic64_t                       state_monitor_head;
 	struct cam_isp_context_state_monitor cam_isp_ctx_state_monitor[
 		CAM_ISP_CTX_STATE_MONITOR_MAX_ENTRIES];
 	bool                             rdi_only_context;
-	atomic_t                         bubble_sof_count;
 };
 
 /**
