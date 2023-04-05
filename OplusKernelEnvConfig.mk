132523a998ae156831b3140e65f959fc58b3fb1c
Dakkshesh <dakkshesh5@gmail.com>
2023-04-05 12:58:18 +0530

    OplusKernelEnvConfig: Shut up
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- OplusKernelEnvConfig.mk
+++ OplusKernelEnvConfig.mk
@@ -78,7 +78,6 @@ OPLUS_FEATURE_DUMPDEVICE
 
 
 $(foreach myfeature,$(ALLOWED_MCROS),\
-         $(warning make $(myfeature) to be a macro here) \
          $(eval KBUILD_CFLAGS += -D$(myfeature)) \
          $(eval KBUILD_CPPFLAGS += -D$(myfeature)) \
          $(eval CFLAGS_KERNEL += -D$(myfeature)) \
