a7c9d44cd9a73ac989d37a01a34cfcc1961a01d3
Dakkshesh <dakkshesh5@gmail.com>
2023-08-26 19:55:16 +0530

    proc: Dont allow Aperture to run in the background
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -1167,7 +1167,8 @@ err_unlock:
 	put_task_struct(task);
 	/* These apps burn through CPU in the background. Don't let them. */
 	if (!err && oom_adj >= 700) {
-		if (!strcmp(task_comm, "id.GoogleCamera")) {
+		if (!strcmp(task_comm, "id.GoogleCamera") ||
+		    !strcmp(task_comm, "eageos.aperture")) {
 			struct task_kill_info *kinfo;
 
 			kinfo = kmalloc(sizeof(*kinfo), GFP_KERNEL);
