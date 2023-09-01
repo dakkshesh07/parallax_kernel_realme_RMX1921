391b760a5e063ca835d5d7b1ab8629322b3bb721
Dakkshesh <dakkshesh5@gmail.com>
2023-09-01 21:38:11 +0530

    kernel: Fix recent commits
    My bad
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -81,6 +81,8 @@
 #include <linux/sched/loadavg.h>
 #include <linux/cgroup-defs.h>
 #include <linux/mutex.h>
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
 
 #include <asm/switch_to.h>
 #include <asm/tlb.h>
