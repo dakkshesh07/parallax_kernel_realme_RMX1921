c04a7f08c8afd6c4852c2acff7081cdff5b9f962
John Galt <johngaltfirstrun@gmail.com>
2023-09-01 19:20:41 +0530

    sched/core: min clamp input boost implementation
    Comparable to userspace pissel libperfmgr implementation.
    
     - Boost top-app min clamp to ~40% within 7s of input
     - Do not touch foreground group: previous of modifying on all max
       boosts was incorrect and harmed many benchmarks due to our process
       fork tracking not being equivalent to userspace app launch tracking
     - Override all min clamps to 0 on battery kprofile (suffer jank for
       efficiency in this mode)
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -941,6 +941,36 @@ static void uclamp_sync_util_min_rt_default(void)
 	rcu_read_unlock();
 }
 
+extern int kp_active_mode(void);
+
+static inline void uclamp_boost_write(struct task_struct *p)
+{
+	struct cgroup_subsys_state *css;
+
+	css = task_css(p, cpu_cgrp_id);
+
+	//top-app min clamp input boost
+	if (strcmp(css->cgroup->kn->name, "top-app") == 0) {
+		if (kp_active_mode() == 3) {
+			task_group(p)->uclamp[UCLAMP_MIN].value = 410;
+			return;
+		}
+		if (time_before(jiffies, last_input_time + msecs_to_jiffies(300))) {
+			task_group(p)->uclamp[UCLAMP_MIN].value = 460;
+			return;
+		} else if (time_before(jiffies, last_input_time + msecs_to_jiffies(3000))) {
+			task_group(p)->uclamp[UCLAMP_MIN].value = 410;
+			return;
+		} else if (time_before(jiffies, last_input_time + msecs_to_jiffies(7000))) {
+			task_group(p)->uclamp[UCLAMP_MIN].value = 205;
+			return;
+		} else {
+			task_group(p)->uclamp[UCLAMP_MIN].value = 102;
+			return;
+		}
+	}
+}
+
 static inline struct uclamp_se
 uclamp_tg_restrict(struct task_struct *p, enum uclamp_id clamp_id)
 {
@@ -958,7 +988,16 @@ uclamp_tg_restrict(struct task_struct *p, enum uclamp_id clamp_id)
 	if (task_group(p) == &root_task_group)
 		return uc_req;
 
-	tg_min = task_group(p)->uclamp[UCLAMP_MIN].value;
+	//battery kprofile optimization
+	if (kp_active_mode() == 1) {
+		tg_min = 0;
+	} else {
+		//Run for clamp boosting
+		uclamp_boost_write(p);
+
+		tg_min = task_group(p)->uclamp[UCLAMP_MIN].value;
+	}
+
 	tg_max = task_group(p)->uclamp[UCLAMP_MAX].value;
 	value = uc_req.value;
 	value = clamp(value, tg_min, tg_max);
