49da451b180979f575bb9fd0d7df78a33daf56ae
Dakkshesh <dakkshesh5@gmail.com>
2023-11-23 19:56:08 +0530

    sched: fair: Make it uclamp aware (properly this time)
    Extension of: https://github.com/dakkshesh07/parallax_kernel_realme_RMX1921/commit/aad5763f4ff6c16b54148e93a821c403fc33cdfd
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6376,7 +6376,11 @@ schedtune_cpu_margin(unsigned long util, int cpu)
 static inline long
 schedtune_task_margin(struct task_struct *p)
 {
+#ifdef CONFIG_SCHED_TUNE
 	int boost = schedtune_task_boost(p);
+#else
+	int boost = uclamp_boosted(p);
+#endif
 	unsigned long util;
 	long margin;
 
@@ -8677,7 +8681,12 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 	 */
 	if (!env->src_rq->rd->overutilized &&
 		env->flags & LBF_IGNORE_BIG_TASKS &&
-		(schedtune_task_boost(p) > 0))
+#ifdef CONFIG_SCHED_TUNE
+		(schedtune_task_boost(p) > 0)
+#else
+		(uclamp_boosted(p) > 0)
+#endif
+		)
 		return 0;
 
 	if (task_running(env->src_rq, p)) {
