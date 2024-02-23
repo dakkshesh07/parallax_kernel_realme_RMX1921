b2573c008666faada3e01a4e09cba0b697477ca5
Dakkshesh <dakkshesh5@gmail.com>
2024-02-23 19:37:01 +0530

    drivers: oppo_touchscreen: Shut up
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- drivers/input/touchscreen/oppo_touchscreen/touchpanel_common_driver.c
+++ drivers/input/touchscreen/oppo_touchscreen/touchpanel_common_driver.c
@@ -42,7 +42,7 @@
 /*******Part0:LOG TAG Declear************************/
 #define TPD_PRINT_POINT_NUM 150
 #define TPD_DEVICE "touchpanel"
-#define TPD_INFO(a, arg...)  pr_info("[TP]"TPD_DEVICE ": " a, ##arg)
+#define TPD_INFO(a, arg...)  pr_debug("[TP]"TPD_DEVICE ": " a, ##arg)
 
 #define TPD_DEBUG(a, arg...) do {} while(0)
 #define TPD_DETAIL(a, arg...) do {} while(0)
